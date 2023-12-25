f48a1c9eff7bee3a71b3c245cb49d1bb4b22f24c
Nick Taylor <nick@nickyt.co>
2023-12-25 15:43:28 -0500

    feat: implemented naive chat

--- party/index.ts
+++ party/index.ts
@@ -1,5 +1,10 @@
 import type * as Party from "partykit/server";
 
+export interface ChatMessage {
+  user: string;
+  text: string;
+}
+
 export default class Server implements Party.Server {
   // eslint-disable-next-line no-unused-vars
   constructor(readonly party: Party.Party) {}
@@ -15,18 +20,22 @@ export default class Server implements Party.Server {
     );
 
     // let's send a message to the connection
-    conn.send("hello from server");
+    conn.send(
+      JSON.stringify({
+        user: "server",
+        text: "Let's get this party started",
+      } as ChatMessage)
+    );
   }
 
   onMessage(message: string, sender: Party.Connection) {
     // let's log the message
     console.log(`connection ${sender.id} sent message: ${message}`);
     // as well as broadcast it to all the other connections in the room...
-    this.party.broadcast(
-      `${sender.id}: ${message}`,
+    this.party.broadcast(message, [
       // ...except for the connection it came from
-      [sender.id]
-    );
+      // sender.id,
+    ]);
   }
 }
 
