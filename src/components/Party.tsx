e3ae51dc8ebd4bb6beef4bbd69045ece6cc0a901
Jessica Wilkins <67210629+jdwilkin4@users.noreply.github.com>
2024-01-28 09:50:54 -0800

    fix: a11y issues for contrast ratios (#56)

--- src/components/Party.tsx
+++ src/components/Party.tsx
@@ -55,7 +55,7 @@ export const Party = ({ username, roomId }: PartyProps) => {
             />
           </label>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             type="submit"
           >
             Send message
